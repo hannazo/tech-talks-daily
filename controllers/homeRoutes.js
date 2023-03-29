bac7126a6e61319e686ce78e422c6be8b08e924a
hannazo <106128732+hannazo@users.noreply.github.com>
2023-03-29 14:34:13 -0400

    added homeRoutes

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,49 @@
+const router = require('express').Router();
+const { User, Post } = require('../models');
+
+// GET all posts for homepage
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+            ],
+        });
+
+        const posts = postData.map((post) =>
+            post.get({ plain: true })
+        );
+        res.render('homepage', {
+            posts,
+            loggedIn: req.session.loggedIn,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// GET one post
+router.get('/post/:id', async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+            ],
+        });
+
+        const post = postData.get({ plain: true });
+        res.render('post', { post, loggedIn: req.session.loggedIn });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
