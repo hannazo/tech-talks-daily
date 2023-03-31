14ec1ecb97e7eb9ecbe5c51d6bb4f4193e614d41
hannazo <106128732+hannazo@users.noreply.github.com>
2023-03-31 18:32:20 -0400

    added login

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,44 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// Login
+router.post('/login', async (req, res) => {
+    try {
+        const dbUserData = await User.findOne({
+            where: {
+                username: req.body.username,
+            },
+        });
+
+        if (!dbUserData) {
+            res
+                .status(400)
+                .json({ message: 'Incorrect username or password. Please try again!' });
+            return;
+        }
+
+        const validPassword = await dbUserData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res
+                .status(400)
+                .json({ message: 'Incorrect username or password. Please try again!' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.logged_in = true;
+
+            res
+                .status(200)
+                .json({ user: dbUserData, message: 'You are now logged in!' });
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(400).json(err);
+    }
+});
+
+module.exports = router;
