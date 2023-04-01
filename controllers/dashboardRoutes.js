fbd4116371ce5dc3def3b4172c30428090f4628e
hannazo <106128732+hannazo@users.noreply.github.com>
2023-04-01 13:55:38 -0400

    added dashboard route

---
+++ controllers/dashboardRoutes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const { User, Post } = require('../models');
+const withAuth = require('../utils/auth');
+
+// GET all posts for dashboard
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            where: {"user_id": req.session.user_id},
+            include: [
+                {
+                    model: User,
+                },
+            ],
+        });
+
+        const posts = postData.map((post) =>
+            post.get({ plain: true })
+        );
+        res.render('all-user-posts', {
+            layout: 'dashboard',
+            posts,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+        res.redirect('login');
+    }
+});
+
+module.exports = router;
