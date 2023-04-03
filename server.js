8c83afd54d85ea7a475d1aa085838e63ff11a116
hannazo <106128732+hannazo@users.noreply.github.com>
2023-04-03 01:43:18 -0400

    minor revisions

--- server.js
+++ server.js
@@ -15,11 +15,16 @@ const hbs = exphbs.create({ helpers });
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: {
+    maxAge: 60 * 60 * 1000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
+    db: sequelize,
   })
 };
 
